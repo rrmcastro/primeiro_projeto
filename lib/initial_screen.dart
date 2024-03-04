ed06042085266b1e6c94c07ab553f27237b83cc1
rrmcastro <ricardormcastro@gmail.com>
2024-03-04 18:52:19 -0300

    Refatorando Task e Tela

---
+++ lib/initial_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:primeiro_projeto/task.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({super.key});
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  bool opacidade = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter: Primeiros Passos'),
+        leading: const Icon(Icons.add_task),
+      ),
+      body: AnimatedOpacity(
+        opacity: opacidade ? 1.0 : 0.0,
+        duration: const Duration(milliseconds: 500),
+        child: Container(
+          color: const Color.fromARGB(255, 208, 221, 237),
+          child: ListView(
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: Tasks(
+                    'Estudar Flutter',
+                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                    3),
+              ),
+              Tasks(
+                  'Andar de Bike',
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4MGk9JbM2t1gqr_rgMPxYym0CUUKG8Z7p1g&usqp=CAU',
+                  2),
+              Tasks(
+                  'Ler 50 páginas',
+                  'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
+                  1),
+              Tasks(
+                  'Meditar',
+                  'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
+                  4),
+              Tasks(
+                  'Jogar',
+                  'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg',
+                  0),
+              SizedBox(
+                height: 100,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            opacidade = !opacidade;
+          });
+        },
+        backgroundColor: Colors.blue[100],
+        child: const Icon(Icons.remove_red_eye),
+      ),
+    );
+  }
+}
