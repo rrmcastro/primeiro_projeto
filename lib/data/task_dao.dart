e942b879590f76947940f0e8101ea0789f6cb461
rrmcastro <ricardormcastro@gmail.com>
2024-03-05 18:44:51 -0300

    Utilizando o Read e Aplicando o toList()

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,4 +1,7 @@
+import 'package:sqflite/sqflite.dart';
+
 import '../components/task.dart';
+import 'database.dart';
 
 class TaskDao {
   static const String tableSql = 'CREATE TABLE $_tablename('
@@ -12,7 +15,32 @@ class TaskDao {
   static const String _image = 'image';
 
   save(Task tarefa) async {}
-  Future<List<Task>> findAll() async {}
+
+  Future<List<Task>> findAll() async {
+    print('Acessando o findAll: ');
+    final Database bancoDeDados = await getDatabase();
+    final List<Map<String, dynamic>> result =
+        await bancoDeDados.query(_tablename);
+    print('Procurando dados no BD... encontrados: $result');
+    return toList(result);
+  }
+
+  List<Task> toList(List<Map<String, dynamic>> mapaDeTarefas) {
+    print('Convertendo toList...');
+    final List<Task> tarefas = [];
+    for (Map<String, dynamic> linha in mapaDeTarefas) {
+      final Task tarefa = Task(
+        linha[_name],
+        linha[_image],
+        linha[_difficulty],
+      );
+      tarefas.add(tarefa);
+    }
+    print('Lista de tarefas: $tarefas');
+    return tarefas;
+  }
+
   Future<List<Task>> find(String nomedaTarefa) async {}
+
   delete(String nomedaTarefa) async {}
 }
