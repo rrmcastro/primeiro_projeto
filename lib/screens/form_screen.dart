ad8ea4b165d5911c909560a14ff1f0b4b0bb2338
rrmcastro <ricardormcastro@gmail.com>
2024-03-05 07:07:16 -0300

    Lidando com errorBuilder

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -28,6 +28,8 @@ class _FormScreenState extends State<FormScreen> {
             border: Border.all(width: 3),
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -58,6 +60,9 @@ class _FormScreenState extends State<FormScreen> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
+                  onChanged: (text) {
+                    setState(() {});
+                  },
                   controller: imageController,
                   textAlign: TextAlign.center,
                   decoration: const InputDecoration(
@@ -65,6 +70,25 @@ class _FormScreenState extends State<FormScreen> {
                     hintText: 'Imagem',
                     fillColor: Colors.white70,
                     filled: true,
+                  ),
+                ),
+              ),
+              Container(
+                height: 90,
+                width: 72,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 2, color: Colors.blue),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    imageController.text,
+                    errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace){
+                      return Image.asset('assets/images/nophoto.png');
+                    },
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
